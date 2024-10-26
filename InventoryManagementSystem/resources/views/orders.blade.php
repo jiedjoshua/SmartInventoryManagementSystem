bf8d297103459369354f7294bebed601000543f1
jaelzzz <157471040+jaelzzz@users.noreply.github.com>
2024-10-27 00:44:36 +0800

    lagay

---
+++ InventoryManagementSystem/resources/views/orders.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="orders.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+
+    <title>Inventory Management</title>
+</head>
+<body>
+<div class="sidenav">
+<div class="logo-container">
+     <h1>Petals & Posies</h1>
+     <p>Inventory Management System</p>
+</div>
+<div class="link-container"> 
+        <a href="#about">Dashboard</a>
+        <button class="dropdown-btn">Inventory Management
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-container">
+            <a href="#1">Flowers</a>
+            <a href="#2">Candles</a>
+            <a href="#3">Orders</a>
+        </div>
+        <a href="#contact">Sales Report</a>
+    </div>
+
+  <div class="logout-container"> <!-- New container for the logout button -->
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            Logout
+        </a>
+    </div>
+</div>
+
+<div class="content">
+        <h2>Orders</h2>
+        <table border="1" style="width:100%; text-align:left; margin-top: 20px;">
+            <tr>
+                <th>Seasonal</th>
+                <th>Trending</th>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+<script src="{{ asset('js/navbar.js') }}"></script>
+</html>
